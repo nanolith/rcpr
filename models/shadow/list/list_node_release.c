f66b4e47461f888a5215255907efd3f1cf7db6a1
Justin Handville <nanolith@gmail.com>
2021-01-08 14:23:42 -0500

    Added model checks for list_insert_head.

---
+++ models/shadow/list/list_node_release.c
@@ -0,0 +1,49 @@
+/**
+ * \file models/shadow/list/list_node_release.c
+ *
+ * \brief Shadow method for list_node_release.
+ *
+ * \copyright 2021 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <rcpr/model_assert.h>
+#include <rcpr/resource.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../../../src/allocator/allocator_internal.h"
+#include "../../../src/list/list_internal.h"
+
+status mock_resource_release(resource* r);
+
+/**
+ * \brief Release an list_node resource.
+ *
+ * \param r             Pointer to the list_node resource to be released.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - an error code on failure.
+ */
+status list_node_release(resource* r)
+{
+    list_node* n = (list_node*)r;
+
+    MODEL_ASSERT(prop_list_node_valid(n));
+    MODEL_ASSERT(NULL == n->parent && NULL == n->next);
+
+    /* if the child resource is set, release it. */
+    if (NULL != n->child)
+    {
+        mock_resource_release(n->child);
+    }
+
+    /* clean up. */
+    allocator* a = n->alloc;
+
+    /* if reclaiming this list_node instance succeeds, so does this release. */
+    return
+        allocator_reclaim(a, n);
+}
