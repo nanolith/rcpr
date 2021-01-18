8ee1f91e09f3ede22db8bd8aac8930682498b400
Justin Handville <nanolith@gmail.com>
2021-01-17 20:40:44 -0500

    Added bigint_create_zero.

---
+++ include/rcpr/bigint.h
@@ -0,0 +1,106 @@
+/**
+ * \file rcpr/bigint.h
+ *
+ * \brief Simple big integer math.
+ *
+ * This provides a simple brute-force big-integer math library.
+ *
+ * \copyright 2021 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <rcpr/allocator.h>
+#include <rcpr/function_decl.h>
+#include <rcpr/resource.h>
+#include <rcpr/status.h>
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+/**
+ * \brief The list container holds zero or more resources in a linear fashion.
+ */
+typedef struct bigint bigint;
+
+/******************************************************************************/
+/* Start of constructors.                                                     */
+/******************************************************************************/
+
+/**
+ * \brief Create a \ref bigint instance of a given size.
+ *
+ * \param i             Pointer to the \ref bigint pointer to receive this
+ *                      resource on success.
+ * \param a             Pointer to the allocator to use for creating this \ref
+ *                      list resource and to allocate new nodes.
+ * \param size          The minimum size of this bigint in bits; the actual
+ *                      representation might be larger to accomodate native
+ *                      integer size.
+ *
+ * \note This \ref bigint is a \ref resource that must be released by calling
+ * \ref resource_release on its resource handle when it is no longer needed by
+ * the caller.  The resource handle can be accessed by calling
+ * \ref bigint_resource_handle on this \ref bigint instance.
+ *
+ * This value will be the bigint equivalent of zero on success.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - ERROR_GENERAL_OUT_OF_MEMORY if this method failed due to an
+ *        out-of-memory condition.
+ *
+ * \pre
+ *      - \p i must not reference a valid \ref bigint instance and must not be
+ *        NULL.
+ *      - \p a must reference a valid \ref allocator and must not be NULL.
+ *
+ * \post
+ *      - On success, \p i is set to a pointer to a valid \ref bigint
+ *        instance, which is a \ref resource owned by the caller that must be
+ *        released.
+ *      - On failure, \p i is set to NULL and an error status is returned.
+ */
+status FN_DECL_MUST_CHECK
+bigint_create_zero(
+    bigint** i, allocator* a, size_t size);
+
+/******************************************************************************/
+/* Start of public methods.                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* Start of accessors.                                                        */
+/******************************************************************************/
+
+/**
+ * \brief Given a \ref bigint instance, return the resource handle for this
+ * \ref bigint instance.
+ *
+ * \param i             The \ref bigint instance from which the resource handle
+ *                      is returned.
+ *
+ * \returns the \ref resource handle for this \ref bigint instance.
+ */
+resource* bigint_resource_handle(bigint* i);
+
+/******************************************************************************/
+/* Start of model checking properties.                                        */
+/******************************************************************************/
+
+/**
+ * \brief Valid \ref bigint property.
+ *
+ * \param i              The \ref bigint instance to be verified.
+ *
+ * \returns true if the \ref bigint instance is valid.
+ */
+bool prop_bigint_valid(const bigint* i);
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
---
+++ src/bigint/bigint_resource_handle.c
@@ -0,0 +1,33 @@
+/**
+ * \file bigint/bigint_resource_handle.c
+ *
+ * \brief Get the resource handle for this bigint.
+ *
+ * \copyright 2021 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <rcpr/model_assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "bigint_internal.h"
+
+/**
+ * \brief Given a \ref bigint instance, return the resource handle for this
+ * \ref bigint instance.
+ *
+ * \param i             The \ref bigint instance from which the resource handle
+ *                      is returned.
+ *
+ * \returns the \ref resource handle for this \ref bigint instance.
+ */
+resource* bigint_resource_handle(bigint* i)
+{
+    /* parameter sanity checks. */
+    MODEL_ASSERT(prop_bigint_valid(i));
+
+    /* return the resource handle for this bigint. */
+    return &i->hdr;
+}
