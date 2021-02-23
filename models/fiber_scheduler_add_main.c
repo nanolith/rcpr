e48c2d6de0e291bad329dd3f9597e1124edb4c2b
Justin Handville <nanolith@gmail.com>
2021-02-22 23:10:53 -0500

    Added model checks for fiber_scheduler_add.

---
+++ models/fiber_scheduler_add_main.c
@@ -0,0 +1,110 @@
+#include <rcpr/allocator.h>
+#include <rcpr/model_assert.h>
+#include <rcpr/resource.h>
+#include <rcpr/fiber.h>
+
+void allocator_struct_tag_init();
+void fiber_struct_tag_init();
+void fiber_scheduler_struct_tag_init();
+void stack_struct_tag_init();
+
+static status callback(
+    void* context, fiber* yield_fib, int yield_event, void* yield_param,
+    fiber** resume_fib, int* resume_event, void** resume_param)
+{
+    MODEL_ASSERT(prop_fiber_valid(yield_fib));
+
+    if (FIBER_SCHEDULER_YIELD_EVENT_MAIN == yield_event)
+    {
+        *resume_fib = yield_fib;
+        *resume_event = FIBER_SCHEDULER_RESUME_EVENT_MAIN;
+        *resume_param = NULL;
+
+        return STATUS_SUCCESS;
+    }
+    else if (FIBER_SCHEDULER_YIELD_EVENT_ADD_FIBER == yield_event)
+    {
+        *resume_fib = yield_fib;
+        *resume_event = FIBER_SCHEDULER_RESUME_EVENT_ADD_FIBER;
+        *resume_param = NULL;
+        fiber* addfib = (fiber*)yield_param;
+        MODEL_ASSERT(prop_fiber_valid(addfib));
+
+        return resource_release(fiber_resource_handle(addfib));
+    }
+    else if (FIBER_SCHEDULER_YIELD_EVENT_RESOURCE_RELEASE == yield_event)
+    {
+        *resume_fib = NULL;
+        *resume_event = FIBER_SCHEDULER_RESUME_EVENT_RESOURCE_RELEASE;
+        *resume_param = NULL;
+
+        return STATUS_SUCCESS;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    allocator* alloc = NULL;
+    fiber_scheduler* sched = NULL;
+    int retval;
+
+    /* set up the global allocator tag. */
+    allocator_struct_tag_init();
+
+    /* set up the global fiber tag. */
+    fiber_struct_tag_init();
+
+    /* set up the global fiber_scheduler tag. */
+    fiber_scheduler_struct_tag_init();
+
+    /* set up the global stack tag. */
+    stack_struct_tag_init();
+
+    /* try to create a malloc allocator. */
+    retval = malloc_allocator_create(&alloc); 
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* create a fiber_scheduler instance. */
+    void* ctx = NULL;
+    retval = fiber_scheduler_create(&sched, alloc, ctx, &callback);
+    if (STATUS_SUCCESS != retval)
+    {
+        /* the only reason why it could fail is due to a memory issue. */
+        MODEL_ASSERT(ERROR_GENERAL_OUT_OF_MEMORY == retval);
+
+        goto cleanup_allocator;
+    }
+
+    /* create a fiber instance. */
+    fiber* fib = NULL;
+    retval = fiber_create_for_thread(&fib, alloc);
+    if (STATUS_SUCCESS != retval)
+    {
+        /* the only reason why it could fail is due to a memory issue. */
+        MODEL_ASSERT(ERROR_GENERAL_OUT_OF_MEMORY == retval);
+
+        goto cleanup_fiber_scheduler;
+    }
+
+    /* add this fiber... */
+    MODEL_ASSERT(STATUS_SUCCESS == fiber_scheduler_add(sched, fib));
+
+cleanup_fiber_scheduler:
+    /* release the fiber_scheduler. */
+    retval = resource_release(fiber_scheduler_resource_handle(sched));
+    MODEL_ASSERT(STATUS_SUCCESS == retval);
+
+cleanup_allocator:
+    /* release the allocator. */
+    resource_release(allocator_resource_handle(alloc));
+
+done:
+    return 0;
+}
