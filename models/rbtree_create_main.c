e52132d8bc6e50548e111e79c0bf5ec226438db6
Justin Handville <nanolith@gmail.com>
2021-03-20 19:17:33 -0400

    Added model checks for rbtree_create.

---
+++ models/rbtree_create_main.c
@@ -0,0 +1,55 @@
+#include <rcpr/allocator.h>
+#include <rcpr/model_assert.h>
+#include <rcpr/resource.h>
+#include <rcpr/rbtree.h>
+
+void allocator_struct_tag_init();
+void rbtree_struct_tag_init();
+
+/* dummy comparison. */
+static rcpr_comparison_result dummy_compare(
+    void* context, const resource* lhs, const resource* rhs)
+{
+    return RCPR_COMPARE_LT;
+}
+
+int main(int argc, char* argv[])
+{
+    allocator* alloc = NULL;
+    rbtree* tree = NULL;
+    int retval;
+
+    /* set up the global allocator tag. */
+    allocator_struct_tag_init();
+
+    /* set up the global rbtree tag. */
+    rbtree_struct_tag_init();
+
+    /* try to create a malloc allocator. */
+    retval = malloc_allocator_create(&alloc); 
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* create an rbtree instance. */
+    retval = rbtree_create(&tree, alloc, &dummy_compare, NULL);
+    if (STATUS_SUCCESS != retval)
+    {
+        /* the only reason why it could fail is due to a memory issue. */
+        MODEL_ASSERT(ERROR_GENERAL_OUT_OF_MEMORY == retval);
+
+        goto cleanup_allocator;
+    }
+
+cleanup_rbtree:
+    /* release the rbtree. */
+    resource_release(rbtree_resource_handle(tree));
+
+cleanup_allocator:
+    /* release the allocator. */
+    resource_release(allocator_resource_handle(alloc));
+
+done:
+    return 0;
+}
