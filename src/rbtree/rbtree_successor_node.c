24f051b5190dc533c2e9dd521b97b3832f2f191f
Justin Handville <nanolith@gmail.com>
2021-04-01 03:02:49 -0400

    Added rbtree_successor_node.

---
+++ src/rbtree/rbtree_successor_node.c
@@ -0,0 +1,39 @@
+/**
+ * \file rbtree/rbtree_successor_node.c
+ *
+ * \brief Return the successor node to a given node.
+ *
+ * \copyright 2021 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <rcpr/model_assert.h>
+#include <string.h>
+
+#include "rbtree_internal.h"
+
+/**
+ * \brief Return the in-order successor node of the given node.
+ *
+ * \param tree          The \ref rbtree instance.
+ * \param x             The \ref rbtree_node from which a successor is found.
+ *
+ * \returns the successor node of this node, or tree->nil if none is found.
+ */
+rbtree_node* rbtree_successor_node(rbtree* tree, rbtree_node* x)
+{
+    if (x->right != tree->nil)
+    {
+        return rbtree_minimum_node(tree, x->right);
+    }
+
+    rbtree_node* y = x->parent;
+
+    while (y != tree->nil && x == y->right)
+    {
+        x = y;
+        y = y->parent;
+    }
+
+    return y;
+}
