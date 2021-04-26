133b031ec7b0a726b9b4832c215d365b373cd931
Justin Handville <nanolith@gmail.com>
2021-04-25 20:15:41 -0400

    Verify that a discipline is not owned before adding it.

--- test/fiber/test_fiber_scheduler_discipline_add.cpp
+++ test/fiber/test_fiber_scheduler_discipline_add.cpp
@@ -158,12 +158,64 @@ TEST(double_add)
         STATUS_SUCCESS ==
             fiber_scheduler_discipline_add(sched, disc));
 
+    /* force the discipline to NOT be owned before calling. */
+    disc->sched = nullptr;
+
     /* Attempting to add the discipline a second time should fail. */
     TEST_ASSERT(
         ERROR_FIBER_SCHEDULER_DUPLICATE_DISCIPLINE_ID ==
             fiber_scheduler_discipline_add(sched, disc));
 
     /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(fiber_scheduler_resource_handle(sched)));
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that adding a discipline fails if the fiber scheduler pointer is
+ * already set.
+ */
+TEST(already_owned)
+{
+    allocator* alloc = nullptr;
+    fiber_scheduler* sched = nullptr;
+    fiber_scheduler_discipline* disc = nullptr;
+    rcpr_uuid id = { .data = {
+        0x0e, 0x2c, 0xfc, 0x92, 0x89, 0xfa, 0x46, 0x54,
+        0xb9, 0x69, 0xd7, 0x1b, 0x18, 0x46, 0x9b, 0x4c } };
+    fiber_scheduler_discipline_callback_fn emptyvec[0];
+
+    /* we should be able to create a malloc allocator. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* we should be able to create a disciplined fiber scheduler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_scheduler_create_with_disciplines(&sched, alloc));
+
+    /* we should be able to create a fiber scheduler discipline. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_scheduler_discipline_create(
+                &disc, &id, alloc, 0, emptyvec));
+
+    /* force the discipline to be owned before calling. */
+    disc->sched = sched;
+
+    /* Adding the discipline should fail because the discipline is already
+     * owned. */
+    TEST_ASSERT(
+        ERROR_FIBER_SCHEDULER_DISCIPLINE_ALREADY_OWNED ==
+            fiber_scheduler_discipline_add(sched, disc));
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(fiber_scheduler_discipline_resource_handle(disc)));
     TEST_ASSERT(
         STATUS_SUCCESS ==
             resource_release(fiber_scheduler_resource_handle(sched)));
