0be27ae3b26c31f55964c63d3b296d81efaa8946
Justin Handville <nanolith@gmail.com>
2021-05-20 00:59:04 -0400

    Added selective compilation for socket events.

---
+++ cmake/psock.cmake
@@ -0,0 +1,20 @@
+#Detect kqueue
+check_symbol_exists(kqueue "sys/types.h;sys/event.h" HAS_PSOCK_ASYNC_KQUEUE)
+check_symbol_exists(epoll_wait "sys/epoll.h" HAS_PSOCK_ASYNC_EPOLL)
+check_symbol_exists(select "sys/select.h" HAS_PSOCK_ASYNC_SELECT)
+
+#prefer kqueue first
+if (HAS_PSOCK_ASYNC_KQUEUE)
+    SET(HAS_PSOCK_ASYNC TRUE)
+    FILE(GLOB RCPR_PSOCK_ASYNC_SOURCES src/psock/platform/kqueue/*.c)
+#prefer epoll next
+elseif (HAS_PSOCK_ASYNC_EPOLL)
+    SET(HAS_PSOCK_ASYNC TRUE)
+    FILE(GLOB RCPR_PSOCK_ASYNC_SOURCES src/psock/platform/epoll/*.c)
+#prefer select last
+elseif (HAS_PSOCK_ASYNC_SELECT)
+    SET(HAS_PSOCK_ASYNC TRUE)
+    FILE(GLOB RCPR_PSOCK_ASYNC_SOURCES src/psock/platform/select/*.c)
+else ()
+    SET(HAS_PSOCK_ASYNC FALSE)
+endif ()
