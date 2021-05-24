11b5436cd3ea1d0e0b7fc41e7b2fb13d4b3e0c0c
Justin Handville <nanolith@gmail.com>
2021-05-24 00:44:50 -0400

    Added code to create and close a kqueue.

--- test/psock/async/test_psock_fiber_scheduler_discipline_create.cpp
+++ test/psock/async/test_psock_fiber_scheduler_discipline_create.cpp
@@ -23,21 +23,27 @@ TEST_SUITE(psock_fiber_scheduler_discipline_create);
 TEST(create)
 {
     allocator* alloc = nullptr;
+    fiber_scheduler* sched = nullptr;
     fiber_scheduler_discipline* disc = nullptr;
 
     /* we should be able to create a malloc allocator. */
     TEST_ASSERT(
         STATUS_SUCCESS == malloc_allocator_create(&alloc));
 
+    /* we should be able to create a fiber scheduler with disciplines. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_scheduler_create_with_disciplines(&sched, alloc));
+
     /* we should be able to create the psock fiber scheduler discipline. */
     TEST_ASSERT(
         STATUS_SUCCESS ==
-            psock_fiber_scheduler_discipline_create(&disc, alloc));
+            psock_fiber_scheduler_discipline_create(&disc, sched, alloc));
 
     /* clean up. */
     TEST_ASSERT(
         STATUS_SUCCESS ==
-            resource_release(fiber_scheduler_discipline_resource_handle(disc)));
+            resource_release(fiber_scheduler_resource_handle(sched)));
     TEST_ASSERT(
         STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
 }
