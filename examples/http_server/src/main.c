8e46ceb9ed10cecb1dbc3d07e97eb59e317035a3
Justin Handville <nanolith@gmail.com>
2021-06-12 00:00:43 -0400

    Added build scaffolding for example HTTP server.

---
+++ examples/http_server/src/main.c
@@ -0,0 +1,16 @@
+/**
+ * \file examples/http_server/src/main.c
+ *
+ * \brief Main entry point for the HTTP server example.
+ *
+ * \copyright 2021 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+/**
+ * \brief Main entry point for the HTTP server example.
+ */
+int main(int argc, char* argv[])
+{
+    return 0;
+}
