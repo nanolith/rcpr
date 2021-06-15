6e9b892f77bbc20c05c41ef9f672d4f562b51df6
Justin Handville <nanolith@gmail.com>
2021-06-15 02:59:29 -0400

    Added versioning and config header.

---
+++ config.h.cmake
@@ -0,0 +1,19 @@
+/**
+ * \file rcpr/config.h
+ *
+ * \brief Generated configuration file data for RCPR.
+ *
+ * \copyright 2021 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#define MAKE_C_VERSION(X,Y) V ## X ## _ ## Y
+#define RCPR_VERSION_SYM \
+    MAKE_C_VERSION(@RCPR_VERSION_MAJOR@, @RCPR_VERSION_MINOR@)
+
+#define RCPR_VERSION_STRING \
+    "@RCPR_VERSION_MAJOR@.@RCPR_VERSION_MINOR@.@RCPR_VERSION_REL@"
+
+#undef MAKE_C_VERSION
