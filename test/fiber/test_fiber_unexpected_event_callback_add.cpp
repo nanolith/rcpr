60eb82a4bd12fafbdd1f86ce455db32f41f301f3
Justin Handville <nanolith@gmail.com>
2021-07-04 16:45:43 -0400

    Fixed unit test for fiber_unexpected_event_callback_add.

--- test/fiber/test_fiber_unexpected_event_callback_add.cpp
+++ test/fiber/test_fiber_unexpected_event_callback_add.cpp
@@ -32,6 +32,7 @@ TEST(add)
     allocator* alloc = nullptr;
     fiber* f = nullptr;
     fiber_scheduler* sched = nullptr;
+    int dummyval = 73;
 
     /* we should be able to create a malloc allocator. */
     TEST_ASSERT(
@@ -47,13 +48,16 @@ TEST(add)
 
     /* PRECONDITION: fiber unexpected event callback not set. */
     TEST_ASSERT(nullptr == f->unexpected_fn);
+    TEST_ASSERT(nullptr == f->unexpected_context);
 
     /* add a custom event callback to this fiber. */
     TEST_ASSERT(
-        STATUS_SUCCESS == fiber_unexpected_event_callback_add(f, &dummy_event));
+        STATUS_SUCCESS ==
+            fiber_unexpected_event_callback_add(f, &dummy_event, &dummyval));
 
     /* POSTCONDITION: fiber unexpected event callback is set. */
     TEST_EXPECT(&dummy_event == f->unexpected_fn);
+    TEST_EXPECT(&dummyval == f->unexpected_context);
 
     /* clean up. */
     TEST_ASSERT(
