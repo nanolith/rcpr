378b766175a5f79757342b3e9ee5630f46d3592d
Justin Handville <nanolith@gmail.com>
2021-07-25 06:50:54 -0400

    Added conditional_notify_all.

--- src/condition/condition_discipline_notify_all_callback_handler.c
+++ src/condition/condition_discipline_notify_all_callback_handler.c
@@ -16,6 +16,8 @@ RCPR_IMPORT_fiber;
 RCPR_IMPORT_condition;
 RCPR_IMPORT_condition_internal;
 RCPR_IMPORT_rbtree;
+RCPR_IMPORT_resource;
+RCPR_IMPORT_slist;
 
 /**
  * \brief The callback handler for a conditional notify all request.
@@ -33,10 +35,72 @@ status RCPR_SYM(condition_discipline_notify_all_callback_handler)(
     void* context, RCPR_SYM(fiber)* yield_fib, int yield_event,
     void* yield_param)
 {
-    (void)context;
-    (void)yield_fib;
-    (void)yield_event;
-    (void)yield_param;
+    status retval;
+    condition_barrier* cond;
+    conditional handle;
+    int resume_event;
+    void* resume_param;
 
-    return -1;
+    /* ignore yield event. */
+    (void)yield_event;
+
+    /* get the condition discipline context. */
+    condition_discipline_context* ctx = (condition_discipline_context*)context;
+
+    /* get the handle. */
+    handle = (conditional)((ptrdiff_t)yield_param);
+
+    /* look up the condition barrier. */
+    retval = rbtree_find((resource**)&cond, ctx->condition_barriers, &handle);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto resume_fail;
+    }
+
+    /* condition barrier sanity check. */
+    RCPR_MODEL_ASSERT(prop_condition_barrier_valid(cond));
+
+    /* for every waiting fiber... */
+    while (slist_count(cond->wait_list) > 0)
+    {
+        /* get a waiting fiber. */
+        fiber* fib;
+        retval = slist_pop(cond->wait_list, (resource**)&fib);
+        if (STATUS_SUCCESS != retval)
+        {
+            /* skip failures. */
+            continue;
+        }
+
+        /* resume the waiting fiber. */
+        retval =
+            disciplined_fiber_scheduler_add_fiber_to_run_queue(
+                ctx->sched, fib, &FIBER_SCHEDULER_CONDITION_DISCIPLINE,
+                FIBER_SCHEDULER_CONDITION_RESUME_EVENT_WAIT_SUCCESS,
+                (void*)((ptrdiff_t)STATUS_SUCCESS));
+        if (STATUS_SUCCESS != retval)
+        {
+            /* skip failures. */
+            continue;
+        }
+    }
+
+    /* success. */
+    goto resume_success;
+
+resume_fail:
+    resume_event = FIBER_SCHEDULER_CONDITION_RESUME_EVENT_NOTIFY_FAILURE;
+    resume_param = (void*)((ptrdiff_t)retval);
+    goto done;
+
+resume_success:
+    resume_event = FIBER_SCHEDULER_CONDITION_RESUME_EVENT_NOTIFY_SUCCESS;
+    resume_param = (void*)((ptrdiff_t)retval);
+    goto done;
+
+done:
+    return
+        disciplined_fiber_scheduler_add_fiber_to_run_queue(
+            ctx->sched, yield_fib, &FIBER_SCHEDULER_CONDITION_DISCIPLINE,
+            resume_event, resume_param);
 }
--- test/condition/test_conditional_wait_notify.cpp
+++ test/condition/test_conditional_wait_notify.cpp
@@ -177,3 +177,189 @@ TEST(notify_one)
         STATUS_SUCCESS ==
             resource_release(allocator_resource_handle(alloc)));
 }
+
+/**
+ * Verify that all waiting fibers can be restarted one at a time by calling
+ * notify_one.
+ */
+TEST(notify_one_multiple)
+{
+    allocator* alloc = nullptr;
+    fiber_scheduler* sched = nullptr;
+    fiber_scheduler_discipline* condisc = nullptr;
+    fiber* fib1 = nullptr;
+    fiber* fib2 = nullptr;
+    conditional cond = -1;
+    test_fiber_context ctx1;
+    test_fiber_context ctx2;
+
+    /* we should be able to create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* we should be able to create a disciplined fiber scheduler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_scheduler_create_with_disciplines(&sched, alloc));
+
+    /* we should be able to create the condition discipline. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            condition_discipline_get_or_create(&condisc, alloc, sched));
+
+    /* we should be able to create a conditional. */
+    TEST_ASSERT(STATUS_SUCCESS == conditional_create(&cond, condisc));
+
+    /* clear the first test context. */
+    memset(&ctx1, 0, sizeof(test_fiber_context));
+
+    /* clear the second test context. */
+    memset(&ctx2, 0, sizeof(test_fiber_context));
+
+    /* set first context values. */
+    ctx1.cond = cond;
+    ctx1.condisc = condisc;
+
+    /* set second context values. */
+    ctx2.cond = cond;
+    ctx2.condisc = condisc;
+
+    /* we should be able to create the first fiber instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_create(
+                &fib1, alloc, sched, 16384, &ctx1, &test_fiber_entry));
+
+    /* we should be able to create the second fiber instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_create(
+                &fib2, alloc, sched, 16384, &ctx2, &test_fiber_entry));
+
+    /* we should be able to add the first fiber to the scheduler. */
+    TEST_ASSERT(STATUS_SUCCESS == fiber_scheduler_add(sched, fib1));
+
+    /* we should be able to add the second fiber to the scheduler. */
+    TEST_ASSERT(STATUS_SUCCESS == fiber_scheduler_add(sched, fib2));
+
+    /* run the scheduler. */
+    TEST_ASSERT(STATUS_SUCCESS == fiber_scheduler_run(sched));
+
+    /* precondition: the first context state should be STARTED. */
+    TEST_ASSERT(TEST_FIBER_STATE_STARTED == ctx1.state);
+
+    /* precondition: the second context state should be STARTED. */
+    TEST_ASSERT(TEST_FIBER_STATE_STARTED == ctx2.state);
+
+    /* notify one fiber to start. */
+    TEST_ASSERT(STATUS_SUCCESS == conditional_notify_one(cond, condisc));
+
+    /* postcondition: the first context state should be FINISHED. */
+    TEST_ASSERT(TEST_FIBER_STATE_FINISHED == ctx1.state);
+
+    /* precondition: the second context state is still STARTED. */
+    TEST_ASSERT(TEST_FIBER_STATE_STARTED == ctx2.state);
+
+    /* notify one fiber to start. */
+    TEST_ASSERT(STATUS_SUCCESS == conditional_notify_one(cond, condisc));
+
+    /* postcondition: the second context state should be FINISHED. */
+    TEST_ASSERT(TEST_FIBER_STATE_FINISHED == ctx2.state);
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(fiber_scheduler_resource_handle(sched)));
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that all waiting fibers can be restarted at once by calling
+ * notify_all.
+ */
+TEST(notify_all)
+{
+    allocator* alloc = nullptr;
+    fiber_scheduler* sched = nullptr;
+    fiber_scheduler_discipline* condisc = nullptr;
+    fiber* fib1 = nullptr;
+    fiber* fib2 = nullptr;
+    conditional cond = -1;
+    test_fiber_context ctx1;
+    test_fiber_context ctx2;
+
+    /* we should be able to create a malloc allocator. */
+    TEST_ASSERT(STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* we should be able to create a disciplined fiber scheduler. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_scheduler_create_with_disciplines(&sched, alloc));
+
+    /* we should be able to create the condition discipline. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            condition_discipline_get_or_create(&condisc, alloc, sched));
+
+    /* we should be able to create a conditional. */
+    TEST_ASSERT(STATUS_SUCCESS == conditional_create(&cond, condisc));
+
+    /* clear the first test context. */
+    memset(&ctx1, 0, sizeof(test_fiber_context));
+
+    /* clear the second test context. */
+    memset(&ctx2, 0, sizeof(test_fiber_context));
+
+    /* set first context values. */
+    ctx1.cond = cond;
+    ctx1.condisc = condisc;
+
+    /* set second context values. */
+    ctx2.cond = cond;
+    ctx2.condisc = condisc;
+
+    /* we should be able to create the first fiber instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_create(
+                &fib1, alloc, sched, 16384, &ctx1, &test_fiber_entry));
+
+    /* we should be able to create the second fiber instance. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            fiber_create(
+                &fib2, alloc, sched, 16384, &ctx2, &test_fiber_entry));
+
+    /* we should be able to add the first fiber to the scheduler. */
+    TEST_ASSERT(STATUS_SUCCESS == fiber_scheduler_add(sched, fib1));
+
+    /* we should be able to add the second fiber to the scheduler. */
+    TEST_ASSERT(STATUS_SUCCESS == fiber_scheduler_add(sched, fib2));
+
+    /* run the scheduler. */
+    TEST_ASSERT(STATUS_SUCCESS == fiber_scheduler_run(sched));
+
+    /* precondition: the first context state should be STARTED. */
+    TEST_ASSERT(TEST_FIBER_STATE_STARTED == ctx1.state);
+
+    /* precondition: the second context state should be STARTED. */
+    TEST_ASSERT(TEST_FIBER_STATE_STARTED == ctx2.state);
+
+    /* notify all fibers to start. */
+    TEST_ASSERT(STATUS_SUCCESS == conditional_notify_all(cond, condisc));
+
+    /* postcondition: the first context state should be FINISHED. */
+    TEST_ASSERT(TEST_FIBER_STATE_FINISHED == ctx1.state);
+
+    /* postcondition: the second context state should be FINISHED. */
+    TEST_ASSERT(TEST_FIBER_STATE_FINISHED == ctx2.state);
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(fiber_scheduler_resource_handle(sched)));
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(allocator_resource_handle(alloc)));
+}
