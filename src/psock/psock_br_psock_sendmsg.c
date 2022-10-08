a8162e21fb0a4c8dad7f763ba02ecbf1ddf0034e
Justin Handville <nanolith@gmail.com>
2022-10-08 03:36:30 -0400

    impl of psock_br_psock_sendmsg.

---
+++ src/psock/psock_br_psock_sendmsg.c
@@ -0,0 +1,36 @@
+/**
+ * \file psock/psock_br_psock_sendmsg.c
+ *
+ * \brief Send a message over the given \ref psock instance.
+ *
+ * \copyright 2022 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include "psock_internal.h"
+
+RCPR_IMPORT_psock;
+RCPR_IMPORT_psock_internal;
+
+/**
+ * \brief Send a message over the \ref psock instance.
+ *
+ * \param sock          The \ref psock instance to which to send a message.
+ * \param msg           Pointer to the message to send.
+ * \param flags         The flags to use when sending the message.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - an error code indicating a specific failure condition.
+ */
+status RCPR_SYM(psock_br_psock_sendmsg)(
+    RCPR_SYM(psock)* sock, const struct msghdr* msg, int flags)
+{
+    psock_br* br = (psock_br*)sock;
+
+    /* parameter sanity checks. */
+    RCPR_MODEL_ASSERT(prop_psock_br_valid(br));
+
+    /* pass through to the wrapped socket. */
+    return br->wrapped->sendmsg_fn(br->wrapped, msg, flags);
+}
