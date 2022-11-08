add9eeeef06eb9e444725f66f20f78759849b4c9
Justin Handville <nanolith@gmail.com>
2022-11-08 04:29:05 -0500

    Workarounds for built-in CBMC types.

--- models/shadow/platform/pthread/pthread/pthread_mutex_destroy.c
+++ models/shadow/platform/pthread/pthread/pthread_mutex_destroy.c
@@ -5,8 +5,9 @@
 
 static int nondet_return_value();
 
-int pthread_mutex_destroy(pthread_mutex_t *mutex)
+int pthread_mutex_destroy(pthread_mutex_t *mu)
 {
+    dummy_pthread_mutex** mutex = (dummy_pthread_mutex**)mu;
     RCPR_MODEL_ASSERT(NULL != mutex);
     RCPR_MODEL_ASSERT(NULL != *mutex);
     RCPR_MODEL_ASSERT(NULL == (*mutex)->lock);
