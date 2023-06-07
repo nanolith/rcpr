1ef818eea2f7393df29e257f0a2e70c13ef3e0d2
Justin Handville <nanolith@gmail.com>
2023-06-07 02:04:17 -0400

    impl of add to interest list for epoll.

--- src/psock/platform/epoll/psock_wrap_async_add_to_interest_list.c
+++ src/psock/platform/epoll/psock_wrap_async_add_to_interest_list.c
@@ -8,7 +8,12 @@
  * distribution for the license terms under which this software is distributed.
  */
 
+#include <rcpr/fiber.h>
+
 #include "psock_epoll_internal.h"
+
+RCPR_IMPORT_fiber;
+RCPR_IMPORT_psock_internal;
 
 /**
  * \brief Instruct the underlying async fiber discipline to add this socket to
@@ -24,7 +29,27 @@
 status RCPR_SYM(psock_wrap_async_add_to_interest_list)(
     RCPR_SYM(psock_wrap_async)* sock)
 {
-    /* currently a no-op for epoll. */
-    (void)sock;
+    status retval;
+    struct epoll_event event;
+    psock_io_epoll_context* ctx =
+        (psock_io_epoll_context*)fiber_scheduler_discipline_context_get(
+            sock->psock_discipline);
+    psock_from_descriptor* pchild = (psock_from_descriptor*)sock->wrapped;
+
+    /* parameter sanity checks. */
+    RCPR_MODEL_ASSERT(prop_psock_wrap_async_valid(sock));
+    RCPR_MODEL_ASSERT(prop_epoll_io_struct_valid(ctx));
+    RCPR_MODEL_ASSERT(PSOCK_TYPE_DESCRIPTOR == sock->wrapped->type);
+
+    /* add this event to the interest list. */
+    event.events = 0;
+    event.data.ptr = NULL;
+    retval = epoll_ctl(ctx->ep, EPOLL_CTL_ADD, pchild->descriptor, &event);
+    if (retval < 0)
+    {
+        return ERROR_PSOCK_EPOLL_CTL_FAILED;
+    }
+
+    /* success. */
     return STATUS_SUCCESS;
 }
