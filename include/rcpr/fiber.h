63c8e135187800c182362b9fe64f73188d1a8f16
Justin Handville <nanolith@gmail.com>
2023-06-10 04:12:29 -0400

    impl for current fiber getter.

--- include/rcpr/fiber.h
+++ include/rcpr/fiber.h
@@ -3,7 +3,7 @@
  *
  * \brief The RCPR fiber library.
  *
- * \copyright 2021 Justin Handville.  Please see license.txt in this
+ * \copyright 2021-2023 Justin Handville.  Please see license.txt in this
  * distribution for the license terms under which this software is distributed.
  */
 
