b8707ed04421039c35f649c02243a0f401f9119b
Justin Handville <nanolith@gmail.com>
2025-03-05 17:02:43 -0500

    psock_create_from_user_methods read unit test.

--- test/psock/test_psock_create_from_user_methods.cpp
+++ test/psock/test_psock_create_from_user_methods.cpp
@@ -24,14 +24,17 @@ TEST_SUITE(psock_create_from_user_methods);
 struct test_psock_context
 {
     bool release_called = false;
+    bool read_called = false;
 };
 
 static status test_psock_release(psock* sock, void* ctx);
+static status test_psock_read(
+    psock* sock, void* ctx, void* data, size_t* size, bool block);
 
 RCPR_VTABLE
 psock_vtable test_psock_vtable = {
     .hdr = { NULL },
-    .read_fn = NULL,
+    .read_fn = &test_psock_read,
     .write_fn = NULL,
     .accept_fn = NULL,
     .sendmsg_fn = NULL,
@@ -46,6 +49,21 @@ static status test_psock_release(psock* sock, void* ctx)
     test_psock_context* test_context = (test_psock_context*)ctx;
 
     test_context->release_called = true;
+
+    return STATUS_SUCCESS;
+}
+
+static status test_psock_read(
+    psock* sock, void* ctx, void* data, size_t* size, bool block)
+{
+    (void)sock;
+    (void)data;
+    (void)size;
+    (void)block;
+
+    test_psock_context* test_context = (test_psock_context*)ctx;
+
+    test_context->read_called = true;
 
     return STATUS_SUCCESS;
 }
@@ -78,6 +96,45 @@ TEST(create)
 
     /* the release method should have been called. */
     TEST_EXPECT(ctx.release_called);
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
+}
+
+/**
+ * Verify that we can read from a user method psock.
+ */
+TEST(read)
+{
+    allocator* alloc = nullptr;
+    psock* s = nullptr;
+    test_psock_context ctx;
+    int32_t data;
+    size_t data_size = sizeof(data);
+
+    /* we should be able to create a malloc allocator. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* we should be able to create a psock from user methods. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            psock_create_from_user_methods(
+                &s, alloc, &ctx, &test_psock_vtable));
+
+    /* we should be able to read from the psock. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == psock_read_raw(s, &data, &data_size));
+
+    /* this calls our function. */
+    TEST_EXPECT(ctx.read_called);
+
+    /* we should be able to release the socket, which in turn will call our
+     * custom release method. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(psock_resource_handle(s)));
 
     /* clean up. */
     TEST_ASSERT(
