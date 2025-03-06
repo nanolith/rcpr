f33a5f20c4396b12be82220394cb2ce613ab2a1a
Justin Handville <nanolith@gmail.com>
2025-03-06 12:57:13 -0500

    Use resource_vtable_read.

--- src/psock/psock_wrap_async_read.c
+++ src/psock/psock_wrap_async_read.c
@@ -54,9 +54,14 @@ status RCPR_SYM(psock_wrap_async_read)(
     RCPR_MODEL_ASSERT(prop_psock_valid(s->wrapped));
 
     /* get the wrapped socket's vtable. */
-    wrapped_vtable =
-        (const psock_vtable*)resource_vtable_get(
+    retval =
+        resource_vtable_read(
+            (const resource_vtable**)&wrapped_vtable,
             psock_resource_handle(s->wrapped));
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
 
     /* loop through until all bytes are read. */
     size_t read_size = *size;
