12bedaf50233f2549f3900abc63d7f67c49dfccc
Justin Handville <nanolith@gmail.com>
2025-03-16 00:41:39 -0400

    RCPR_BEGIN_CONTRACT_HELPER decl.

---
+++ include/rcpr/function_contracts.h
@@ -0,0 +1,18 @@
+/**
+ * \file rcpr/function_contracts.h
+ *
+ * \brief Function contract macros for handling preconditions, postconditions,
+ * and model checks.
+ *
+ * \copyright 2024 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+/**
+ * Begin a contract helper section.
+ */
+#define RCPR_BEGIN_CONTRACT_HELPER \
+    _Pragma("GCC diagnostic push") \
+    _Pragma("GCC diagnostic ignored \"-Wunused-parameter\"")
