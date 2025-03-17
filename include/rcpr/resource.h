6529bdbd5192c6e724fc5c111f0afa2b570b1ad3
Justin Handville <nanolith@gmail.com>
2025-03-17 04:24:33 -0400

    resource_release function contract preconditions.

--- include/rcpr/resource.h
+++ include/rcpr/resource.h
@@ -11,7 +11,9 @@
 
 #pragma once
 
+#include <rcpr/function_contracts.h>
 #include <rcpr/function_decl.h>
+#include <rcpr/model_assert.h>
 #include <rcpr/status.h>
 #include <stdbool.h>
 
@@ -88,6 +90,14 @@ RCPR_SYM(prop_resource_valid)(
 status FN_DECL_MUST_CHECK
 RCPR_SYM(resource_release)(
     RCPR_SYM(resource)* r);
+
+/* preconditions. */
+RCPR_MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    RCPR_SYM(resource_release),
+    RCPR_SYM(resource)* r)
+        /* this must be a valid resource. */
+        RCPR_MODEL_ASSERT(RCPR_SYM(prop_resource_valid)(r));
+RCPR_MODEL_CONTRACT_PRECONDITIONS_END(RCPR_SYM(resource_release))
 
 /******************************************************************************/
 /* Start of public types.                                                     */
