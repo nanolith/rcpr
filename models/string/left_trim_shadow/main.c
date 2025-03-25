23db869c5bdc52f277c3310c7e10365a87000750
Justin Handville <nanolith@gmail.com>
2025-03-25 02:28:44 -0400

    left_trim shadow model checks.

---
+++ models/string/left_trim_shadow/main.c
@@ -0,0 +1,18 @@
+#include <rcpr/model_assert.h>
+#include <rcpr/string.h>
+
+RCPR_IMPORT_string_as(rcpr);
+
+int main(int argc, char* argv[])
+{
+    /* a string of random data. */
+    char buffer[8];
+    buffer[7] = 0;
+    RCPR_MODEL_ASSUME(__CPROVER_is_zero_string(buffer));
+
+    /* left_trim should work as expected. */
+    rcpr_left_trim(buffer);
+
+    /* success. */
+    return 0;
+}
