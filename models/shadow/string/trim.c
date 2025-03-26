e20cc94a458ffd645a3466114fe5193b79f73baf
Justin Handville <nanolith@gmail.com>
2025-03-26 01:56:14 -0400

    Fixed function contract enforcement in shadow impl.

--- models/shadow/string/trim.c
+++ models/shadow/string/trim.c
@@ -15,9 +15,9 @@ RCPR_IMPORT_string_as(rcpr);
 
 void RCPR_SYM(trim)(char* str)
 {
-    RCPR_MODEL_CONTRACT_CHECK_PRECONDITIONS(RCPR_SYM(left_trim), str);
+    RCPR_MODEL_CONTRACT_CHECK_PRECONDITIONS(RCPR_SYM(trim), str);
 
     RCPR_MODEL_ASSUME(!rcpr_is_whitespace(str[0]));
 
-    RCPR_MODEL_CONTRACT_CHECK_POSTCONDITIONS(RCPR_SYM(left_trim), str);
+    RCPR_MODEL_CONTRACT_CHECK_POSTCONDITIONS(RCPR_SYM(trim), str);
 }
