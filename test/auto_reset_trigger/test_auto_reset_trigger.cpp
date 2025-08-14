80c13de8daa32c0b1c514374d97cf7c4b1b08817
Justin Handville <nanolith@gmail.com>
2025-08-13 22:42:50 -0400

    auto_reset_trigger test suite.

---
+++ test/auto_reset_trigger/test_auto_reset_trigger.cpp
@@ -0,0 +1,14 @@
+/**
+ * \file test/auto_reset_trigger/test_auto_reset_trigger.cpp
+ *
+ * \brief Unit tests for auto_reset_trigger.
+ */
+
+#include <minunit/minunit.h>
+#include <rcpr/auto_reset_trigger.h>
+
+using namespace std;
+
+RCPR_IMPORT_auto_reset_trigger;
+
+TEST_SUITE(auto_reset_trigger);
