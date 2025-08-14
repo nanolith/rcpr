43fa8443f75d53f087e6f8500c8bea0594119b0b
Justin Handville <nanolith@gmail.com>
2025-08-14 00:35:45 -0400

    auto_reset_trigger create unit test.

--- test/auto_reset_trigger/test_auto_reset_trigger.cpp
+++ test/auto_reset_trigger/test_auto_reset_trigger.cpp
@@ -5,10 +5,39 @@
  */
 
 #include <minunit/minunit.h>
+#include <rcpr/allocator.h>
 #include <rcpr/auto_reset_trigger.h>
 
 using namespace std;
 
+RCPR_IMPORT_allocator;
 RCPR_IMPORT_auto_reset_trigger;
+RCPR_IMPORT_resource;
 
 TEST_SUITE(auto_reset_trigger);
+
+/**
+ * Verify that we can create and release a \ref auto_reset_trigger instance.
+ */
+TEST(create)
+{
+    allocator* alloc = nullptr;
+    auto_reset_trigger* trigger = nullptr;
+
+    /* we should be able to create a malloc allocator. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == malloc_allocator_create(&alloc));
+
+    /* we should be able to create an auto_reset_trigger. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == auto_reset_trigger_create(&trigger, alloc));
+
+    /* we should be able to release the trigger. */
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            resource_release(auto_reset_trigger_resource_handle(trigger)));
+
+    /* clean up. */
+    TEST_ASSERT(
+        STATUS_SUCCESS == resource_release(allocator_resource_handle(alloc)));
+}
