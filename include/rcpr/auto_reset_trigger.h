1bd24ae83e0974fc9a3d6db032b7c289c088e845
Justin Handville <nanolith@gmail.com>
2025-08-13 22:42:36 -0400

    auto_reset_trigger decl.

---
+++ include/rcpr/auto_reset_trigger.h
@@ -0,0 +1,41 @@
+/**
+ * \file rcpr/auto_reset_trigger.h
+ *
+ * \brief An auto-reset trigger is a one-shot trigger that resets on
+ * (potentially delayed) notification.
+ *
+ * \copyright 2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <rcpr/function_decl.h>
+
+/* C++ compatibility. */
+# ifdef    __cplusplus
+extern "C" {
+# endif /* __cplusplus */
+
+/**
+ * \brief The auto-reset trigger.
+ */
+typedef struct RCPR_SYM(auto_reset_trigger) RCPR_SYM(auto_reset_trigger);
+
+/******************************************************************************/
+/* Start of public exports.                                                   */
+/******************************************************************************/
+#define __INTERNAL_RCPR_IMPORT_auto_reset_trigger_sym(sym) \
+    RCPR_BEGIN_EXPORT \
+    typedef RCPR_SYM(auto_reset_trigger) sym ## auto_reset_trigger; \
+    RCPR_END_EXPORT \
+    REQUIRE_SEMICOLON_HERE
+#define RCPR_IMPORT_auto_reset_trigger_as(sym) \
+    __INTERNAL_RCPR_IMPORT_auto_reset_trigger_sym(sym ## _)
+#define RCPR_IMPORT_auto_reset_trigger \
+    __INTERNAL_RCPR_IMPORT_auto_reset_trigger_sym()
+
+/* C++ compatibility. */
+# ifdef    __cplusplus
+}
+# endif /* __cplusplus */
