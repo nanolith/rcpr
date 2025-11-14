5bb6312166e40297c84f8cbe07a9b9a0d97fcf5c
Justin Handville <nanolith@gmail.com>
2025-11-14 03:45:58 -0500

    auto_reset_trigger_resource_handle function contract postconditions.

--- include/rcpr/auto_reset_trigger.h
+++ include/rcpr/auto_reset_trigger.h
@@ -233,6 +233,14 @@ RCPR_MODEL_CONTRACT_PRECONDITIONS_BEGIN(
 RCPR_MODEL_CONTRACT_PRECONDITIONS_END(
     RCPR_SYM(auto_reset_trigger_resource_handle))
 
+/* postconditions. */
+RCPR_MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    RCPR_SYM(auto_reset_trigger_resource_handle), RCPR_SYM(resource)* retval)
+        /* a valid resource handle is returned. */
+        RCPR_MODEL_ASSERT(RCPR_SYM(prop_resource_valid)(retval));
+RCPR_MODEL_CONTRACT_POSTCONDITIONS_END(
+    RCPR_SYM(auto_reset_trigger_resource_handle))
+
 /******************************************************************************/
 /* Start of public exports.                                                   */
 /******************************************************************************/
