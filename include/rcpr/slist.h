5f361d37a9c491776b0eaf4e8e3cb3bd1cfe90c9
Justin Handville <nanolith@gmail.com>
2025-11-15 23:47:13 -0500

    slist_clear function contract preconditions.

--- include/rcpr/slist.h
+++ include/rcpr/slist.h
@@ -553,6 +553,13 @@ RCPR_MODEL_CONTRACT_POSTCONDITIONS_END(RCPR_SYM(slist_swap))
 status FN_DECL_MUST_CHECK
 RCPR_SYM(slist_clear)(RCPR_SYM(slist)* l);
 
+/* preconditions. */
+RCPR_MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    RCPR_SYM(slist_clear), RCPR_SYM(slist)* l)
+        /* l is a valid slist. */
+        RCPR_MODEL_ASSERT(property_slist_valid(l));
+RCPR_MODEL_CONTRACT_PRECONDITIONS_END(RCPR_SYM(slist_clear))
+
 /******************************************************************************/
 /* Start of accessors.                                                        */
 /******************************************************************************/
