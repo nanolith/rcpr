921d4b5073a7c86775cfb6d353206b7471bd45f7
Justin Handville <nanolith@gmail.com>
2025-11-15 22:37:57 -0500

    slist_append_tail function contract postconditions.

--- include/rcpr/slist.h
+++ include/rcpr/slist.h
@@ -213,6 +213,19 @@ RCPR_MODEL_CONTRACT_PRECONDITIONS_BEGIN(
         RCPR_MODEL_ASSERT(prop_resource_valid(r));
 RCPR_MODEL_CONTRACT_PRECONDITIONS_END(RCPR_SYM(slist_append_tail))
 
+/* postconditions. */
+RCPR_MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    RCPR_SYM(slist_append_tail), status retval, RCPR_SYM(slist)* l,
+    RCPR_SYM(resource)* r)
+        /* l is a valid slist. */
+        RCPR_MODEL_ASSERT(property_slist_valid(l));
+        /* This method either succeeds or fails with
+         * ERROR_GENERAL_OUT_OF_MEMORY. */
+        RCPR_MODEL_ASSERT(
+            (STATUS_SUCCESS == retval)
+         || (ERROR_GENERAL_OUT_OF_MEMORY == retval));
+RCPR_MODEL_CONTRACT_POSTCONDITIONS_END(RCPR_SYM(slist_append_tail))
+
 /**
  * \brief Append the given \ref resource to the next value of the given \ref
  * slist_node.
