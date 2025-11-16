51aaea3c041af117ec15101acaa07695443af180
Justin Handville <nanolith@gmail.com>
2025-11-15 22:36:54 -0500

    slist_append_tail function contract preconditions.

--- include/rcpr/slist.h
+++ include/rcpr/slist.h
@@ -204,6 +204,15 @@ status FN_DECL_MUST_CHECK
 RCPR_SYM(slist_append_tail)(
     RCPR_SYM(slist)* list, RCPR_SYM(resource)* r);
 
+/* preconditions. */
+RCPR_MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    RCPR_SYM(slist_append_tail), RCPR_SYM(slist)* l, RCPR_SYM(resource)* r)
+        /* l is a valid slist. */
+        RCPR_MODEL_ASSERT(property_slist_valid(l));
+        /* r is a valid resource. */
+        RCPR_MODEL_ASSERT(prop_resource_valid(r));
+RCPR_MODEL_CONTRACT_PRECONDITIONS_END(RCPR_SYM(slist_append_tail))
+
 /**
  * \brief Append the given \ref resource to the next value of the given \ref
  * slist_node.
