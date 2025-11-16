e995c38d38113a413ba967b5bc306d3582005ee4
Justin Handville <nanolith@gmail.com>
2025-11-15 23:23:26 -0500

    slist_node_child_swap function contract preconditions.

--- include/rcpr/slist.h
+++ include/rcpr/slist.h
@@ -317,6 +317,22 @@ status FN_DECL_MUST_CHECK
 RCPR_SYM(slist_node_child_swap)(
     RCPR_SYM(slist_node)* node, RCPR_SYM(resource)** r);
 
+/* preconditions. */
+RCPR_MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    RCPR_SYM(slist_node_child_swap), RCPR_SYM(slist_node)* node,
+    RCPR_SYM(resource)** r)
+        /* node is a valid slist node. */
+        RCPR_MODEL_ASSERT(property_slist_node_valid(node));
+        /* r is a valid pointer. */
+        RCPR_MODEL_CHECK_OBJECT_RW(r, sizeof(*r));
+        /* if *r is not NULL... */
+        if (NULL != *r)
+        {
+            /* *r is a valid resource. */
+            RCPR_MODEL_ASSERT(prop_resource_valid(*r));
+        }
+RCPR_MODEL_CONTRACT_PRECONDITIONS_END(RCPR_SYM(slist_node_child_swap))
+
 /**
  * \brief Pop the head value of the list, setting the given resource pointer to
  * the resource previously held in the head node.
