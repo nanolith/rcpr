d174a302fd81dce1b7cb89ab54b3f37fcabbb65f
Justin Handville <nanolith@gmail.com>
2025-11-15 23:53:00 -0500

    slist_node_resource_handle function contract preconditions.

--- include/rcpr/slist.h
+++ include/rcpr/slist.h
@@ -612,6 +612,13 @@ RCPR_SYM(resource)*
 RCPR_SYM(slist_node_resource_handle)(
     RCPR_SYM(slist_node)* node);
 
+/* preconditions. */
+RCPR_MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    RCPR_SYM(slist_node_resource_handle), RCPR_SYM(slist_node)* node)
+        /* node is a valid slist node. */
+        RCPR_MODEL_ASSERT(property_slist_node_valid(node));
+RCPR_MODEL_CONTRACT_PRECONDITIONS_END(RCPR_SYM(slist_node_resource_handle))
+
 /**
  * \brief Get the head of a \ref slist.
  *
