844a2583e8524f4cbe61d804b49f0e7b0b5aa62e
Justin Handville <nanolith@gmail.com>
2025-11-16 00:34:14 -0500

    list structure shadow impl.

---
+++ models/shadow/list/list_shadow.h
@@ -0,0 +1,24 @@
+/**
+ * \file rcpr/models/shadow/list/list_shadow.h
+ *
+ * \brief Shadow header for RCPR list.
+ *
+ * \copyright 2020-2025 Justin Handville.  Please see license.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+#include <rcpr/list.h>
+
+struct RCPR_SYM(list)
+{
+    RCPR_SYM(resource) hdr;
+
+    RCPR_MODEL_STRUCT_TAG(list);
+
+    RCPR_SYM(allocator)* alloc;
+    RCPR_SYM(list_node)* head;
+    RCPR_SYM(list_node)* tail;
+    size_t count;
+};
