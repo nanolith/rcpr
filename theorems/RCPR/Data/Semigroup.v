e36833d8128da6bd681aae3fb2a2b13af5a65c66
Justin Handville <nanolith@gmail.com>
2025-11-19 00:42:50 -0500

    Added <o> operator for semigroup op.

--- theorems/RCPR/Data/Semigroup.v
+++ theorems/RCPR/Data/Semigroup.v
@@ -6,4 +6,11 @@ Class Semigroup (S: Type -> Type) : Type := {
     op : forall {a : Type}, S a -> S a -> S a;
 }.
 
+Declare Scope semigroup_scope.
+
+Delimit Scope semigroup_scope with semigroup.
+
+(* The <o> operator maps to op. *)
+Infix "<o>" := op (at level 65, left associativity) : semigroup_scope.
+
 End Semigroup.
