4cfe8574513fe0682bd996babd34a92c01a8b486
Justin Handville <nanolith@gmail.com>
2025-11-19 04:09:06 -0500

    Added functor_id law.

--- theorems/RCPR/Data/Functor.v
+++ theorems/RCPR/Data/Functor.v
@@ -3,6 +3,8 @@ Module Functor.
 (* The Functor provides a way to map a function into a functor space. *)
 Class Functor (F : Type -> Type) := {
     fmap : forall {A B : Type} , (A -> B) -> F A -> F B;
+    functor_id : forall {A : Type} (x : F A),
+        fmap (fun a => a) x = x;
 }.
 
 End Functor.
