b52130bdde3e4b327eac86f2870198b866d3e9d8
Justin Handville <nanolith@gmail.com>
2025-11-19 00:45:02 -0500

    Added implicit arguments for Semigroup::op.

--- theorems/RCPR/Data/Semigroup.v
+++ theorems/RCPR/Data/Semigroup.v
@@ -13,4 +13,7 @@ Delimit Scope semigroup_scope with semigroup.
 (* The <o> operator maps to op. *)
 Infix "<o>" := op (at level 65, left associativity) : semigroup_scope.
 
+(* Gather the implicit type A parameter from implicit context. *)
+Arguments op {A} {_} _ _ : rename.
+
 End Semigroup.
