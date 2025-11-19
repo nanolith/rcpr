97e6618bd99583a373962ea1ed8f3327e44a9d38
Justin Handville <nanolith@gmail.com>
2025-11-19 10:14:17 -0500

    Functor identity proof.

--- theorems/RCPR/Theorems/Functor.v
+++ theorems/RCPR/Theorems/Functor.v
@@ -4,4 +4,20 @@ Import Functor.
 
 Module Functor.
 
+Section FunctorTheories.
+    Variable F : Type -> Type.
+    Context `{Functor F}.
+
+    Open Scope functor_scope.
+
+    (* verify functor identity. *)
+    Lemma identity : forall {A : Type} (x : F A),
+        (fun a => a) ⟨$⟩ x = x.
+    Proof.
+        intros.
+        apply functor_id.
+    Qed.
+
+End FunctorTheories.
+
 End Functor.
