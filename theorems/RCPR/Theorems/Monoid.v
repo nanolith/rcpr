3a8ce36891e8f4a0fa0fb80febb92f5f6456d8a7
Justin Handville <nanolith@gmail.com>
2025-11-19 01:57:17 -0500

    Added identity_right lemma.

--- theorems/RCPR/Theorems/Monoid.v
+++ theorems/RCPR/Theorems/Monoid.v
@@ -1,7 +1,25 @@
 Require Import RCPR.Data.Monoid.
+Require Import RCPR.Data.Semigroup.
 
 Import Monoid.
+Import Semigroup.
 
 Module Monoid.
+
+Section MonoidTheories.
+    Variable S : Type -> Type.
+    Context `{Monoid S}.
+
+    Open Scope semigroup_scope.
+
+    (* verify right associative identity. *)
+    Lemma identity_right : forall {t : Type} (x : S t),
+        x <o> mempty = x.
+    Proof.
+        intros.
+        apply mempty_right.
+    Abort.
+
+End MonoidTheories.
 
 End Monoid.
