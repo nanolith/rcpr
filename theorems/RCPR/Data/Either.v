12a9f6d4ef04fcd8365b599d494224f6143b113d
Justin Handville <nanolith@gmail.com>
2025-11-18 21:55:13 -0500

    Added Either type.

---
+++ theorems/RCPR/Data/Either.v
@@ -0,0 +1,13 @@
+Module Either.
+
+(* The Either inductive type describes a disjunctive value. *)
+Inductive Either (A B : Type) : Type :=
+    | Left : A -> Either A B
+    | Right : B -> Either A B.
+
+(* The implicit type A maps to the Left type. *)
+Arguments Left {A B} _ , [A] B _.
+(* The implicit type B maps to the Right type. *)
+Arguments Right {A B} _ , A [B] _.
+
+End Either.
