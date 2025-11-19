254560416605cc45dea20ed90b95069b2d88ad91
Justin Handville <nanolith@gmail.com>
2025-11-19 01:51:43 -0500

    Added mempty_right and mempty_left theorems to Monoid.

--- theorems/RCPR/Data/Monoid.v
+++ theorems/RCPR/Data/Monoid.v
@@ -6,8 +6,10 @@ Module Monoid.
 
 (* The Monoid typeclass provides a way to describe a binary operation with *)
 (* identity. *)
-Class Monoid M `{S : Semigroup M} : Type := {
+Class Monoid (M : Type -> Type) `{S : Semigroup M} := {
     mempty : forall {t : Type}, M t;
+    mempty_right : forall {t : Type} (x : M t), op x mempty = x;
+    mempty_left : forall {t : Type} (x : M t), op mempty x = x;
 }.
 
 End Monoid.
