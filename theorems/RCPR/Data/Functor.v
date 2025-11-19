60c331de98baec3a0ad71323426a6d099e63978a
Justin Handville <nanolith@gmail.com>
2025-11-19 04:12:50 -0500

    Added functor_comp law.

--- theorems/RCPR/Data/Functor.v
+++ theorems/RCPR/Data/Functor.v
@@ -5,6 +5,8 @@ Class Functor (F : Type -> Type) := {
     fmap : forall {A B : Type} , (A -> B) -> F A -> F B;
     functor_id : forall {A : Type} (x : F A),
         fmap (fun a => a) x = x;
+    functor_comp : forall {A B C : Type} (f : A -> B) (g : B -> C) (x : F A),
+        fmap (fun y => g (f y)) x = fmap g (fmap f x)
 }.
 
 End Functor.
