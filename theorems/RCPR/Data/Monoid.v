ffb71d336ceeee4f85c2199cee661aa76a72c221
Justin Handville <nanolith@gmail.com>
2025-11-20 08:10:26 -0500

    Use ∀ notation.

--- theorems/RCPR/Data/Monoid.v
+++ theorems/RCPR/Data/Monoid.v
@@ -1,5 +1,7 @@
 Require Import RCPR.Data.Semigroup.
+Require Import RCPR.Helpers.Notation.
 
+Import Notation.
 Import Semigroup.
 
 Module Monoid.
@@ -7,9 +9,9 @@ Module Monoid.
 (* The Monoid typeclass provides a way to describe a binary operation with *)
 (* identity. *)
 Class Monoid (M : Type -> Type) `{S : Semigroup M} := {
-    mempty : forall {t : Type}, M t;
-    mempty_right : forall {t : Type} (x : M t), op x mempty = x;
-    mempty_left : forall {t : Type} (x : M t), op mempty x = x;
+    mempty : ∀ {t : Type}, M t;
+    mempty_right : ∀ {t : Type} (x : M t), op x mempty = x;
+    mempty_left : ∀ {t : Type} (x : M t), op mempty x = x;
 }.
 
 End Monoid.
