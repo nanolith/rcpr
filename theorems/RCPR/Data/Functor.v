c198a535aced628d06153afa78c808a79b6e80f5
Justin Handville <nanolith@gmail.com>
2025-11-20 08:27:57 -0500

    Use → notation for implication.

--- theorems/RCPR/Data/Functor.v
+++ theorems/RCPR/Data/Functor.v
@@ -5,11 +5,11 @@ Import Notation.
 Module Functor.
 
 (* The Functor provides a way to map a function into a functor space. *)
-Class Functor (F : Type -> Type) := {
-    fmap : ∀ {A B : Type} , (A -> B) -> F A -> F B;
+Class Functor (F : Type → Type) := {
+    fmap : ∀ {A B : Type} , (A → B) → F A → F B;
     functor_id : ∀ {A : Type} (x : F A),
         fmap (fun a => a) x = x;
-    functor_comp : ∀ {A B C : Type} (f : A -> B) (g : B -> C) (x : F A),
+    functor_comp : ∀ {A B C : Type} (f : A → B) (g : B → C) (x : F A),
         fmap (fun y => g (f y)) x = fmap g (fmap f x)
 }.
 
