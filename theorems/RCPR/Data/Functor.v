3045cf7759713134b957feb1c412769f797fb5b8
Justin Handville <nanolith@gmail.com>
2025-11-20 08:10:11 -0500

    Use ∀ notation.

--- theorems/RCPR/Data/Functor.v
+++ theorems/RCPR/Data/Functor.v
@@ -1,11 +1,15 @@
+Require Import RCPR.Helpers.Notation.
+
+Import Notation.
+
 Module Functor.
 
 (* The Functor provides a way to map a function into a functor space. *)
 Class Functor (F : Type -> Type) := {
-    fmap : forall {A B : Type} , (A -> B) -> F A -> F B;
-    functor_id : forall {A : Type} (x : F A),
+    fmap : ∀ {A B : Type} , (A -> B) -> F A -> F B;
+    functor_id : ∀ {A : Type} (x : F A),
         fmap (fun a => a) x = x;
-    functor_comp : forall {A B C : Type} (f : A -> B) (g : B -> C) (x : F A),
+    functor_comp : ∀ {A B C : Type} (f : A -> B) (g : B -> C) (x : F A),
         fmap (fun y => g (f y)) x = fmap g (fmap f x)
 }.
 
