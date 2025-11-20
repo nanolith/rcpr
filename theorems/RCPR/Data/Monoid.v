4aac0b7bed4518d4bdd534298a1bff4562a3ac71
Justin Handville <nanolith@gmail.com>
2025-11-20 08:28:41 -0500

    Use → notation for implication.

--- theorems/RCPR/Data/Monoid.v
+++ theorems/RCPR/Data/Monoid.v
@@ -8,7 +8,7 @@ Module Monoid.
 
 (* The Monoid typeclass provides a way to describe a binary operation with *)
 (* identity. *)
-Class Monoid (M : Type -> Type) `{S : Semigroup M} := {
+Class Monoid (M : Type → Type) `{S : Semigroup M} := {
     mempty : ∀ {t : Type}, M t;
     mempty_right : ∀ {t : Type} (x : M t), op x mempty = x;
     mempty_left : ∀ {t : Type} (x : M t), op mempty x = x;
