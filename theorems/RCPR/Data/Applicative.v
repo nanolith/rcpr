e0aa48e1b603d0612397d60a8aeecbff2f3c4478
Justin Handville <nanolith@gmail.com>
2025-11-20 08:09:52 -0500

    Use ∀ notation.

--- theorems/RCPR/Data/Applicative.v
+++ theorems/RCPR/Data/Applicative.v
@@ -1,10 +1,14 @@
+Require Import RCPR.Helpers.Notation.
+
+Import Notation.
+
 Module Applicative.
 
 (* The Applicative Functor provides a way to lift both functions and values *)
 (* to a functor space. *)
 Class Applicative (A : Type -> Type) := {
-    pure : forall {t : Type}, t -> A t;
-    app : forall {a b : Type}, A (a -> b) -> A a -> A b
+    pure : ∀ {t : Type}, t -> A t;
+    app : ∀ {a b : Type}, A (a -> b) -> A a -> A b
 }.
 
 Declare Scope applicative_scope.
