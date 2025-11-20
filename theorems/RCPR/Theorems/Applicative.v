321d59687149b33797bcde448f08a94d68f89349
Justin Handville <nanolith@gmail.com>
2025-11-20 12:30:55 -0500

    Applicative is a functor with fmap identity.

--- theorems/RCPR/Theorems/Applicative.v
+++ theorems/RCPR/Theorems/Applicative.v
@@ -1,7 +1,9 @@
 Require Import RCPR.Data.Applicative.
+Require Import RCPR.Data.Functor.
 Require Import RCPR.Helpers.Notation.
 
 Import Applicative.
+Import Functor.
 Import Notation.
 
 Module Applicative.
@@ -11,6 +13,7 @@ Section ApplicativeTheories.
     Context `{Applicative A}.
 
     Open Scope applicative_scope.
+    Open Scope functor_scope.
 
     (* verify ap identity. *)
     Lemma ap_identity : ∀ {t : Type} (v : A t),
@@ -18,6 +21,15 @@ Section ApplicativeTheories.
     Proof.
         intros.
         apply ap_id.
+    Qed.
+
+    (* verify ap identity using fmap. *)
+    Lemma ap_identity_fmap : ∀ {t : Type} (v : A t),
+        (λ x ↦ x) ⟨$⟩ v = v.
+    Proof.
+        intros.
+        unfold fmap.
+        apply functor_id.
     Qed.
 End ApplicativeTheories.
 
