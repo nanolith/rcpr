5dc1e0108648f3181430cd71f671bb64004d9f2c
Justin Handville <nanolith@gmail.com>
2025-11-20 08:10:47 -0500

    Use ∀ notation.

--- theorems/RCPR/Data/Semigroup.v
+++ theorems/RCPR/Data/Semigroup.v
@@ -1,10 +1,14 @@
+Require Import RCPR.Helpers.Notation.
+
+Import Notation.
+
 Module Semigroup.
 
 (* The Semigroup typeclass provides a single computation operation that has *)
 (* associativity. *)
 Class Semigroup (S: Type -> Type) := {
-    op : forall {a : Type}, S a -> S a -> S a;
-    op_assoc : forall {a : Type} (x y z : S a), op x (op y z) = op (op x y) z
+    op : ∀ {a : Type}, S a -> S a -> S a;
+    op_assoc : ∀ {a : Type} (x y z : S a), op x (op y z) = op (op x y) z
 }.
 
 Declare Scope semigroup_scope.
