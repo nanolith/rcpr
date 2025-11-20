1c6e78fb871dc9e37ff0d9b82825813954e437aa
Justin Handville <nanolith@gmail.com>
2025-11-20 12:07:23 -0500

    ap_identity lemma.

--- theorems/RCPR/Theorems/Applicative.v
+++ theorems/RCPR/Theorems/Applicative.v
@@ -1,7 +1,24 @@
 Require Import RCPR.Data.Applicative.
+Require Import RCPR.Helpers.Notation.
 
 Import Applicative.
+Import Notation.
 
 Module Applicative.
+
+Section ApplicativeTheories.
+    Variable A : Type → Type.
+    Context `{Applicative A}.
+
+    Open Scope applicative_scope.
+
+    (* verify ap identity. *)
+    Lemma ap_identity : ∀ {t : Type} (v : A t),
+        pure (λ x ↦ x) ⊛ v = v.
+    Proof.
+        intros.
+        apply ap_id.
+    Qed.
+End ApplicativeTheories.
 
 End Applicative.
