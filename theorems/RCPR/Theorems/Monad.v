f71d082ccd27d58716b560a3917f9e06813e27ba
Justin Handville <nanolith@gmail.com>
2025-11-21 10:12:02 -0500

    Monad theory stub.

---
+++ theorems/RCPR/Theorems/Monad.v
@@ -0,0 +1,13 @@
+Require Import RCPR.Data.Applicative.
+Require Import RCPR.Data.Monad.
+Require Import RCPR.Data.Functor.
+Require Import RCPR.Helpers.Notation.
+
+Import Applicative.
+Import Functor.
+Import Monad.
+Import Notation.
+
+Module Monad.
+
+End Monad.
