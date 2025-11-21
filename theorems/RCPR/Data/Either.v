bee30114bb138c1c095b0ec63612d2bbc86d6b72
Justin Handville <nanolith@gmail.com>
2025-11-21 04:42:11 -0500

    Added Functor instance for Either.

--- theorems/RCPR/Data/Either.v
+++ theorems/RCPR/Data/Either.v
@@ -1,5 +1,7 @@
+Require Import RCPR.Data.Functor.
 Require Import RCPR.Helpers.Notation.
 
+Import Functor.
 Import Notation.
 
 Module Either.
@@ -13,5 +15,28 @@ Inductive Either (A B : Type) : Type :=
 Arguments Left {A B} _ , [A] B _.
 (* The implicit type B maps to the Right type. *)
 Arguments Right {A B} _ , A [B] _.
+
+(* The Either Applicative instance provides us with an error Applicative. *)
+Program Instance EitherFunctor (E : Type) : Functor (Either E) := {
+    fmap := λ A B f x ↦ 
+        match x with
+        | Left e => Left e
+        | Right a => Right (f a)
+        end;
+}.
+(* Proof of id law. *)
+Next Obligation.
+    intros.
+    destruct x.
+    reflexivity.
+    reflexivity.
+Qed.
+(* Proof of composition law. *)
+Next Obligation.
+    intros.
+    destruct x.
+    reflexivity.
+    reflexivity.
+Qed.
 
 End Either.
