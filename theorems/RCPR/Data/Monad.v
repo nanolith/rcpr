8e6585a9a37ac6ac448b256766a1ac17124672a0
Justin Handville <nanolith@gmail.com>
2025-11-21 22:58:55 -0500

    Fixed up Monad implicits.

--- theorems/RCPR/Data/Monad.v
+++ theorems/RCPR/Data/Monad.v
@@ -10,21 +10,18 @@ Module Monad.
 (* operator space. *)
 Class Monad (M : Type → Type) `{Applicative M} := {
     bind : ∀ {A B : Type}, M A → (A → M B) → M B;
-    ret : ∀ {t : Type}, t → M t;
+    ret : ∀ {A : Type}, A → M A;
 
     (* Left identity. *)
     monad_left_id : ∀ {A B : Type} (x : A) (f : A → M B),
         bind (ret x) f = f x;
 }.
 
-Arguments bind {M} {_} {A B} m f : rename.
-Arguments ret  {M} {_} {A} x : rename.
-
 Declare Scope monad_scope.
 Delimit Scope monad_scope with monad.
 
 (* The ▶ operator maps to bind. *)
-Infix "▶" := bind (at level 65, left associativity) : monad_scope.
+Notation "f ▶ g" := (bind f g) (at level 65, left associativity) : monad_scope.
 
 (* The » operator maps to "do". *)
 Notation "f » g" :=
