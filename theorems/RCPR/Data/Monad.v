f74f6ece97e4513a6acbe0b6955f4c7e11e800d5
Justin Handville <nanolith@gmail.com>
2025-11-23 19:18:15 -0500

    Fixed comment.

--- theorems/RCPR/Data/Monad.v
+++ theorems/RCPR/Data/Monad.v
@@ -6,7 +6,7 @@ Import Notation.
 
 Module Monad.
 
-(* The Monady provides a way to lift functions and effects into Monadic *)
+(* The Monad provides a way to lift functions and effects into Monadic *)
 (* operator space. *)
 Class Monad (M : Type → Type) `{Applicative M} := {
     bind : ∀ {A B : Type}, M A → (A → M B) → M B;
