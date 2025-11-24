48d6072f21c6280a04edc62e3099352e4f4bc209
Justin Handville <nanolith@gmail.com>
2025-11-24 06:47:25 -0500

    Remove MonadTrans, since Rocq is more strict types.

--- theorems/RCPR/Data/Monad/Transformer.v
+++ theorems/RCPR/Data/Monad/Transformer.v
@@ -6,9 +6,4 @@ Import Notation.
 
 Module Transformer.
 
-(* MonadTrans provides a way to stack Monad instances. *)
-Class MonadTrans (T: (Type → Type) → Type → Type) := {
-    lift : ∀ {M : Type → Type} `{Monad M} {A : Type}, M A → T M A;
-}.
-
 End Transformer.
