eeb5f6735baae3e9abe6bf20af8737758139a6af
Justin Handville <nanolith@gmail.com>
2025-11-27 02:55:20 -0500

    CLinkedList inductive type.

--- theorems/list/CMachine.v
+++ theorems/list/CMachine.v
@@ -5,3 +5,7 @@ Import Maybe.
 (* Simulated Linked List node in C. *)
 Inductive CLinkedListNode : Type :=
 | Node (prev : Maybe nat) (next : Maybe nat) (val : nat).
+
+(* Simulated Linked List in C. *)
+Inductive CLinkedList : Type :=
+| List (head : Maybe nat) (tail : Maybe nat) (count : nat).
