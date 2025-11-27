1af2ec47b2e9a5637569391b584dd3102c6b4cd6
Justin Handville <nanolith@gmail.com>
2025-11-27 02:55:56 -0500

    CMemoryLocation type.

--- theorems/list/CMachine.v
+++ theorems/list/CMachine.v
@@ -9,3 +9,11 @@ Inductive CLinkedListNode : Type :=
 (* Simulated Linked List in C. *)
 Inductive CLinkedList : Type :=
 | List (head : Maybe nat) (tail : Maybe nat) (count : nat).
+
+(* Simulated Memory Location in C. *)
+Inductive CMemoryLocation : Type :=
+| CMemUninit (loc : nat)
+| CMemNode (loc: nat) (node : CLinkedListNode)
+| CMemList (loc: nat) (list : CLinkedList)
+| CMemNodePtr (loc : nat) (ptr : nat)
+| CMemListPtr (loc : nat) (ptr : nat).
