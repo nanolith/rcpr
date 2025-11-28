744b81ca7f4ab3f4730a05e51312dce0cb9c855d
Justin Handville <nanolith@gmail.com>
2025-11-28 09:12:40 -0500

    Verify that if load fails, casting to list node fails.

--- theorems/list/CMachineTheorems.v
+++ theorems/list/CMachineTheorems.v
@@ -1,7 +1,25 @@
+Require Import RCPR.Data.Monad.
+Require Import RCPR.Helpers.Notation.
 Require Import list.CMachine.
 
 Import CMachine.
+Import Monad.
+Import Notation.
 
 Module CMachineTheorems.
+
+(* Verify that if a location fails to load, it can't be cast to a node. *)
+Lemma loadLinkedListNode_MachineErrorLoad :
+    ∀ (n : nat) (l : CLocal) (h : CHeap) (addr : nat),
+            loadRaw addr n l h = MachineError MachineErrorLoad →
+            loadLinkedListNode addr n l h = MachineError MachineErrorLoad.
+Proof.
+    intros n l h addr H.
+    unfold loadLinkedListNode.
+    unfold bind, MachineMMonad.
+    simpl.
+    rewrite H.
+    reflexivity.
+Qed.
 
 End CMachineTheorems.
