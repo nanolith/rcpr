7322365f724ff30e9a8f73ecbb56f849de843bfc
Justin Handville <nanolith@gmail.com>
2025-11-28 10:34:54 -0500

    If a location fails to load, it cannot be coerced to a node pointer.

--- theorems/list/CMachineTheorems.v
+++ theorems/list/CMachineTheorems.v
@@ -176,4 +176,18 @@ Proof.
     reflexivity.
 Qed.
 
+(* Verify that if a location fails to load, it can't be cast to a node ptr. *)
+Lemma loadLinkedListNodePtr_MachineErrorLoad :
+    ∀ (n : nat) (l : CLocal) (h : CHeap) (addr : nat),
+            loadRaw addr n l h = MachineError MachineErrorLoad →
+            loadLinkedListNodePtr addr n l h = MachineError MachineErrorLoad.
+Proof.
+    intros n l h addr H.
+    unfold loadLinkedListNodePtr.
+    unfold bind, MachineMMonad.
+    simpl.
+    rewrite H.
+    reflexivity.
+Qed.
+
 End CMachineTheorems.
