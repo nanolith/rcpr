0c2d5529695463f34fcb17b3aa7da8595286b01d
Justin Handville <nanolith@gmail.com>
2025-11-28 09:16:42 -0500

    Can't coerce linked list to linked list node.

--- theorems/list/CMachineTheorems.v
+++ theorems/list/CMachineTheorems.v
@@ -25,12 +25,26 @@ Qed.
 End CMachineTheorems.
 
 (* Can't coerce uninitialized memory to LinkedListNode. *)
-Lemma loadLinkedListNode_MachineErrorCast :
+Lemma loadLinkedListNode_Uninit_MachineErrorCast :
     ∀ (n : nat) (l : CLocal) (h : CHeap) (addr : nat),
             loadRaw addr n l h = MachineState n l h (CMemUninit addr) →
             loadLinkedListNode addr n l h = MachineError MachineErrorCast.
 Proof.
     intros n l h addr H.
+    unfold loadLinkedListNode.
+    unfold bind, MachineMMonad.
+    simpl.
+    rewrite H.
+    reflexivity.
+Qed.
+
+(* Can't coerce LinkedList to LinkedListNode. *)
+Lemma loadLinkedListNode_LinkedList_MachineErrorCast :
+    ∀ (n : nat) (l : CLocal) (h : CHeap) (addr : nat) (x : CLinkedList),
+            loadRaw addr n l h = MachineState n l h (CMemList addr x) →
+            loadLinkedListNode addr n l h = MachineError MachineErrorCast.
+Proof.
+    intros n l h addr x H.
     unfold loadLinkedListNode.
     unfold bind, MachineMMonad.
     simpl.
