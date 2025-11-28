e4e30f657e9db8d05f0a03d14333884693e7d938
Justin Handville <nanolith@gmail.com>
2025-11-28 11:42:15 -0500

    Happy path for loadRaw.

--- theorems/list/CMachineTheorems.v
+++ theorems/list/CMachineTheorems.v
@@ -29,6 +29,23 @@ Proof.
     reflexivity.
 Qed.
 
+(* If a memory lookup succeeds, loadRaw returns this value. *)
+Lemma loadRaw_rw :
+    ∀ (n : nat) (l : CLocal) (h : CHeap) (values : IList CMemoryLocation)
+      (addr : nat) (cell : CMemoryLocation),
+            getHeapMemory n l h = MachineState n l h values →
+            lookupMem addr values = Just cell →
+            loadRaw addr n l h = MachineState n l h cell.
+Proof.
+    intros n l h values addr cell H1 H2.
+    unfold loadRaw.
+    unfold bind, MachineMMonad.
+    simpl.
+    rewrite H1.
+    rewrite H2.
+    reflexivity.
+Qed.
+
 (* Verify that if a location fails to load, it can't be cast to a node. *)
 Lemma loadLinkedListNode_MachineErrorLoad :
     ∀ (n : nat) (l : CLocal) (h : CHeap) (addr : nat),
