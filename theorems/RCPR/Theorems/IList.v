53f03567c55399215cbc8e03da7ced68d4e9c140
Justin Handville <nanolith@gmail.com>
2025-11-28 16:05:04 -0500

    Lemma to rewrite a reverse as a peel.

--- theorems/RCPR/Theorems/IList.v
+++ theorems/RCPR/Theorems/IList.v
@@ -1,7 +1,19 @@
 Require Import RCPR.Data.IList.
+Require Import RCPR.Helpers.Notation.
 
 Import IList.
+Import Notation.
 
 Module IList.
+
+(* We can peel the first item to the end of a reversed list. *)
+Lemma IList_reverse_peel :
+    ∀ {A} (x : A) (l : IList A),
+        reverse (x :: l) = (reverse l) ++ [x].
+Proof.
+    intros A x l.
+    unfold reverse.
+    reflexivity.
+Qed.
 
 End IList.
