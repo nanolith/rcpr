716f41ce00f9748d63141ada11d7252b6cdc3ee7
Justin Handville <nanolith@gmail.com>
2025-12-04 18:51:40 -0500

    CMachineLoadTheorems stub.

---
+++ theorems/list/CMachineLoadTheorems.v
@@ -0,0 +1,21 @@
+Require Import RCPR.Data.IList.
+Require Import RCPR.Data.Maybe.
+Require Import RCPR.Data.Monad.
+Require Import RCPR.Helpers.Notation.
+Require Import RCPR.Theorems.IListTheorems.
+Require Import RCPR.Theorems.NatTheorems.
+Require Import list.CMachine.
+
+Import CMachine.
+Import IList.
+Import IListTheorems.
+Import Maybe.
+Import Monad.
+Import NatTheorems.
+Import Notation.
+
+Module CMachineLoadTheorems.
+
+Open Scope monad_scope.
+
+End CMachineLoadTheorems.
