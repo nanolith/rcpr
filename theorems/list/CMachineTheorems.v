6e7f01750148de3f9b928c5d7e5248b09021b01e
Justin Handville <nanolith@gmail.com>
2025-12-04 19:39:29 -0500

    Moved loadLinkedListPtr_rw.

--- theorems/list/CMachineTheorems.v
+++ theorems/list/CMachineTheorems.v
@@ -20,20 +20,6 @@ Module CMachineTheorems.
 
 Open Scope monad_scope.
 
-(* Happy path: we can load linked list pointers. *)
-Lemma loadLinkedListPtr_rw :
-    ∀ (n : nat) (l : CLocal) (h : CHeap) (addr : nat) (x : Maybe nat),
-            loadRaw addr n l h = MachineState n l h (CMemListPtr addr x) →
-            loadLinkedListPtr addr n l h = MachineState n l h x.
-Proof.
-    intros n l h addr x H.
-    unfold loadLinkedListPtr.
-    unfold bind, MachineMMonad.
-    simpl.
-    rewrite H.
-    reflexivity.
-Qed.
-
 (* Verify that if a location fails to load, it can't be a node ptr ptr. *)
 Lemma loadLinkedListNodePtrPtr_MachineErrorLoad :
     ∀ (n : nat) (l : CLocal) (h : CHeap) (addr : nat),
