b980808ceed2f8bce4abed77e07dcc41b2ad96f0
Justin Handville <nanolith@gmail.com>
2025-12-19 00:28:45 -0500

    INS_SetListNodePrev notation.

--- theorems/list/CMachine.v
+++ theorems/list/CMachine.v
@@ -219,6 +219,11 @@ Notation "'@setListNodeNext' '(' localAddr ',' nextAddr ')' ';' next" :=
     (in custom c_lang at level 3, localAddr constr, nextAddr constr,
      next custom c_lang at level 200).
 
+Notation "'@setListNodePrev' '(' localAddr ',' prevAddr ')' ';' next" :=
+    (INS_SetListNodePrev localAddr prevAddr next)
+    (in custom c_lang at level 3, localAddr constr, prevAddr constr,
+     next custom c_lang at level 200).
+
 (* Machine State. *)
 Inductive Machine (A : Type) :=
 | MachineError : MachineErrorCode → Machine A
