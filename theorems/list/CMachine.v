c36e83b615d6f2043de02201f6a6ad557b816b0b
Justin Handville <nanolith@gmail.com>
2025-12-29 18:21:00 -0500

    Added notation for INS_AssignLocalListNodePtrToHeapListNodePrev.

--- theorems/list/CMachine.v
+++ theorems/list/CMachine.v
@@ -225,6 +225,11 @@ Notation "'@assignLocalListNodePtrToHeapListNodeNext' '(' localAddr ',' heapAddr
     (in custom c_lang at level 3, localAddr constr, heapAddr constr,
      next custom c_lang at level 200).
 
+Notation "'@assignLocalListNodePtrToHeapListNodePrev' '(' localAddr ',' heapAddr ')' ';' next" :=
+    (INS_AssignLocalListNodePtrToHeapListNodePrev localAddr heapAddr next)
+    (in custom c_lang at level 3, localAddr constr, heapAddr constr,
+     next custom c_lang at level 200).
+
 Notation "'@checkHeapListPtrAddress' '(' heapAddr ')' ';' next" :=
     (INS_CheckHeapListPtrAddress heapAddr next)
     (in custom c_lang at level 3, heapAddr constr,
