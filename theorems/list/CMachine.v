78af86ea798c80df904f8897d595a81a2c7cf590
Justin Handville <nanolith@gmail.com>
2025-12-31 22:06:03 -0500

    Added notation for INS_SetLocalListTailNull.

--- theorems/list/CMachine.v
+++ theorems/list/CMachine.v
@@ -343,6 +343,11 @@ Notation "'@setLocalListHeadNull' '(' localAddr ')' ';' next" :=
     (in custom c_lang at level 3, localAddr constr,
      next custom c_lang at level 200).
 
+Notation "'@setLocalListTailNull' '(' localAddr ')' ';' next" :=
+    (INS_SetLocalListTailNull localAddr next)
+    (in custom c_lang at level 3, localAddr constr,
+     next custom c_lang at level 200).
+
 (* Machine State. *)
 Inductive Machine (A : Type) :=
 | MachineError : MachineErrorCode → Machine A
