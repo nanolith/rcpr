1dfac970d97d7f6c41732aef7610b2354ff1b158
Justin Handville <nanolith@gmail.com>
2025-12-31 22:11:40 -0500

    evalCreateLocalLinkedListNodePtr impl.

--- theorems/list/CMachine.v
+++ theorems/list/CMachine.v
@@ -1288,6 +1288,15 @@ Definition evalCreateLocalLinkedListPtrPtr (addr : nat) : MachineM unit :=
     else
         throw MachineErrorBadInstruction.
 
+(* Evaluate a create local linked list node ptr instruction. *)
+Definition evalCreateLocalLinkedListNodePtr (addr : nat) : MachineM unit :=
+    createLocalLinkedListNodePtr Nothing ▶
+    λ localAddr ↦
+    if Nat.eqb localAddr addr then
+        ret tt
+    else
+        throw MachineErrorBadInstruction.
+
 (* Evaluate a create linked list instruction. *)
 Definition evalCreateLinkedList (localAddr : nat) : MachineM unit :=
     maybeCreateLinkedList ▶
