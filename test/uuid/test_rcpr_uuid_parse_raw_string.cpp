bbc9fd27dcd628ef8f637a63284fc47060349a93
Justin Handville <nanolith@gmail.com>
2026-04-25 19:04:34 -0400

    Happy path test for uuid raw string parse.

--- test/uuid/test_rcpr_uuid_parse_raw_string.cpp
+++ test/uuid/test_rcpr_uuid_parse_raw_string.cpp
@@ -52,3 +52,22 @@ TEST(large_string)
             rcpr_uuid_parse_raw_string(
                 &id, STR, strlen(STR)));
 }
+
+/**
+ * Happy path.
+ */
+TEST(happy_path)
+{
+    rcpr_uuid id;
+    const char* STR = "a8362615-46f1-46ff-9ce3-f43249145d57";
+    const rcpr_uuid EXPECTED_UUID = { .data = {
+        0xa8, 0x36, 0x26, 0x15, 0x46, 0xf1, 0x46, 0xff,
+        0x9c, 0xe3, 0xf4, 0x32, 0x49, 0x14, 0x5d, 0x57 } };
+
+    TEST_ASSERT(
+        STATUS_SUCCESS ==
+            rcpr_uuid_parse_raw_string(
+                &id, STR, strlen(STR)));
+
+    TEST_EXPECT(!memcmp(&id, &EXPECTED_UUID, sizeof(id)));
+}
