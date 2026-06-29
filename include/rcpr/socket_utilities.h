55b0d9a21e77d12dcc50e9dbaf000de84ae2f27d
Justin Handville <nanolith@gmail.com>
2026-06-28 20:46:33 -0400

    socket_utility_setfds decl.

--- include/rcpr/socket_utilities.h
+++ include/rcpr/socket_utilities.h
@@ -63,6 +63,21 @@ status FN_DECL_MUST_CHECK
 RCPR_SYM(socket_utility_move_descriptors)(int threshold, int* desc, ...);
 
 /**
+ * \brief Set the offsets of the given file descriptors to the provided mapped
+ * values.
+ *
+ * \note This method should be called after \ref socket_utility_move_descriptors
+ * in order to set custom descriptors to the expected values for a child
+ * process.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a failure code on failure.
+ */
+status FN_DECL_MUST_CHECK
+RCPR_SYM(socket_utility_setfds)(int curr, int mapped, ...);
+
+/**
  * \brief Set a descriptor to non-blocking.
  *
  * \param desc              The descriptor to set to non-blocking.
