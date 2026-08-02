4ce81deb7459f7e0348b537a4e0f6d1c6da91c4d
Justin Handville <nanolith@gmail.com>
2026-08-02 17:26:56 -0400

    psock_create_socketpair decl.

--- include/rcpr/psock.h
+++ include/rcpr/psock.h
@@ -640,6 +640,56 @@ RCPR_SYM(psock_create_from_user_methods)(
     void* context, const RCPR_SYM(psock_vtable)* vtable);
 
 /**
+ * \brief Create a pair of \ref psock instances backed by a connected pair of
+ * sockets with the given domain, type, and protocol.
+ *
+ * \param lhs           Pointer to the \ref psock pointer to receive the
+ *                      left-hand-side of this socket pair on success.
+ * \param rhs           Pointer to the \ref psock pointer to receive the
+ *                      right-hand-side of this socket pair on success.
+ * \param a             Pointer to the allocator to use for creating this
+ *                      socket pair.
+ * \param domain        The domain for this socket pair.
+ * \param type          The type of this socket pair.
+ * \param protocol      The protocol for this socket pair.
+ *
+ * \note These \ref psock instances are both \ref resource instances that must
+ * be released by calling \ref resource_release on their resource handles when
+ * they are no longer needed by the caller.  The resource handle for these
+ * instances can be accessed by calling \ref psock_resource_handle.
+ *
+ * The \ref psock instances created are assumed to be backed by a blocking
+ * stream socket, and any read / write operations on these sockets will behave
+ * accordingly.  On platforms which support this, \ref psock_create_wrap_async
+ * can be called to wrap these \ref psock instances with an asynchronous I/O
+ * instance.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a error code on failure.
+ *
+ * \pre
+ *      - \p lhs must not reference a valid sock instance and must not be NULL.
+ *      - \p rhs must not reference a valid sock instance and must not be NULL.
+ *      - \p a must reference a valid \ref allocator and must not be NULL.
+ *      - \p domain must refer to a valid socket domain.
+ *      - \p type must refer to a valid socket type.
+ *      - \p protocol must refer to a valid protocol for this socket domain and
+ *        type, or must be 0.
+ *
+ * \post
+ *      - On success, \p lhs and \p rhs are set to pointers to valid \ref psock
+ *        instances, which are \ref resource instances owned by the caller that
+ *        must be released.
+ *      - On failure, \p lhs and \p rhs are  set to NULL and an error status is
+ *        returned.
+ */
+status FN_DECL_MUST_CHECK
+RCPR_SYM(psock_create_socketpair)(
+    RCPR_SYM(psock)** lhs, RCPR_SYM(psock)** rhs, RCPR_SYM(allocator)* a,
+    int domain, int type, int protocol);
+
+/**
  * \brief Create a \ref psock_br instance backed by the given \ref psock
  * instance.
  *
