ac28f11f4a1bf64ebb0421984614b89ba154a870
Justin Handville <nanolith@gmail.com>
2026-09-19 20:10:37 -0400

    Ensure that data clear can't be optimized out.

--- src/queue/queue_create.c
+++ src/queue/queue_create.c
@@ -76,7 +76,7 @@ RCPR_SYM(queue_create)(
     }
 
     /* clear structure. */
-    memset(tmp, 0, sizeof(queue));
+    explicit_bzero(tmp, sizeof(queue));
 
     /* the tag is not set by default. */
     RCPR_MODEL_ASSERT_STRUCT_TAG_NOT_INITIALIZED(
